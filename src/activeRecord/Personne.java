417f76d9661f4e8f597140f518f2c540d86e10c8
matias amaglio <matiasamaglio@gmail.com>
2023-11-22 15:47:33 +0100

    Singleton DBco + début Personne

---
+++ src/activeRecord/Personne.java
@@ -0,0 +1,16 @@
+package activeRecord;
+
+public class Personne {
+
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public Personne(int id, String nom, String prenom) {
+        this.id = -1;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    
+}
